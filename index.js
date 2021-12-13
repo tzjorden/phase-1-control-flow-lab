596f8d2a0cfcf91d7c5a8e6a5777c6f8dcb12fad
Tenzin Jorden <tenden96@gmail.com>
2021-12-13 14:56:28 -0800

    commit tree b6db2806ad5d993e84cdbc3fa9c2f8038fe99688

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+    if(distance <= 400){
+      return "This one is on me!";
+    }
+    if(distance >= 2000 && distance <=2499){
+        return "I will gladly take your thirty bucks.";
+      }
+    if(distance > 2500){
+          return "No can do.";
+        }
 }
-
+  
 function ternaryCheckCity(){
-  // Write your code here!
+    // Write your code here!
+    
+    let city = "NYC";
+    
+    console.log((city = "NYC") ? "Ok, sounds good." : "No go.");
+    
+    
+
+
+
+
+}
+  
+function switchOnCharmFromTip(){
+    // Write your code here!
+    //let generous;
+    //let notGenerous;
+
+    switch(tip){
+        case 'generous' :
+            return "Thank you so much.";
+            break;
+        case 'notGenerous' :
+            return "Thank you.";
+            break;
+        default :
+            return "Bye.";
+    }
+
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
